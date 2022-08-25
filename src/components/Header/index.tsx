e91b304b227a71a801f02369eed811735543e0c4
yousaf wazir <umbrellaman612@gmail.com>
2022-08-25 17:04:18 +0100

    feat: header fully added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import { Logo } from './Logo'
 import { MobileMenuHandler } from './Mobile/MobileMenuHandler'
 import { SearchMenuHandler } from './Search/SearchMenuHandler'
+import { NavOptions } from './Values/NavOptions'
 
 interface indexProps {}
 
@@ -9,6 +11,16 @@ export const Header: React.FC<indexProps> = () => {
     <header>
       <nav className="sticky top-0 flex items-center justify-between w-full h-20 px-5 mx-auto border-b shadow-md max-w-screen-2xl border-b-slate-300">
         <Logo />
+        {/** Desktop options */}
+        <div className='items-center hidden gap-2 p-1 md:flex'>
+          {NavOptions.map((Option) => (
+            <Link href={`/${Option.href}`} key={Option.title}>
+              <a className={`${Option.moreOptions && 'hidden'} border border-gray-200 shadow-md px-3 py-2 rounded-md`}>
+                {Option.title}
+              </a>
+            </Link>
+          ))}
+        </div>
         <div className="flex items-center gap-3">
           <SearchMenuHandler />
           <MobileMenuHandler />
