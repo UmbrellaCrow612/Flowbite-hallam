93042b12c32f3cb3513a589b58d7f767dad6fc67
yousaf wazir <umbrellaman612@gmail.com>
2022-08-30 08:51:35 +0100

    fix: the loading screen head

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,8 +1,36 @@
+import Head from 'next/head'
+
 interface LoadingScreenProps {}
 
 export const LoadingScreen: React.FC<LoadingScreenProps> = () => {
   // https://10015.io/tools/css-loader-generator use the cubic one
-    return (
+  return (
+    <>
+      <Head>
+        <title>Sheffield Hallam University</title>
+        <meta
+          name="description"
+          content="The official University website, Sheffield Hallam University, Home page. "
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
       <section className="flex items-center justify-center min-h-screen">
         <div className="spinner">
           <div></div>
@@ -13,5 +41,6 @@ export const LoadingScreen: React.FC<LoadingScreenProps> = () => {
           <div></div>
         </div>
       </section>
-    )
+    </>
+  )
 }
